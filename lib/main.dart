914f5418267da23d9368323721de0cdb2aa1690d
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-12-21 13:47:31 -0300

    :sparkles: add transfer list and floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,24 @@ void main() {
         appBar: AppBar(
           title: Text('Transferências'),
         ),
+        body: Column(
+          children: [
+            ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100.0'),
+              subtitle: Text('1000'),
+            ),
+            ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('200.0'),
+              subtitle: Text('2000'),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
     ),
   );
