8b8b5807096ec2952e7a5e81f1be22157bf7f770
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-12-21 16:13:45 -0300

    :recycle: refactoring transfers

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,7 @@ void main() {
         appBar: AppBar(
           title: Text('Transferências'),
         ),
-        body: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.0'),
-              subtitle: Text('1000'),
-            ),
-            ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200.0'),
-              subtitle: Text('2000'),
-            ),
-          ],
-        ),
+        body: Transfers(),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Icon(Icons.add),
@@ -28,4 +15,47 @@ void main() {
       ),
     ),
   );
+}
+
+class Transfers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <TransferCard>[
+        TransferCard(Transfer(value: 100.0, accountNumber: 123123123)),
+        TransferCard(Transfer(value: 110.0, accountNumber: 123123123)),
+        TransferCard(Transfer(value: 200.0, accountNumber: 222222222)),
+      ],
+    );
+  }
+}
+
+class TransferCard extends StatelessWidget {
+  const TransferCard(
+    this._transfer, {
+    Key key,
+  }) : super(key: key);
+
+  final Transfer _transfer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text('${_transfer.value}'),
+        subtitle: Text('Destinatário: ${_transfer.accountNumber}'),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer({
+    @required this.value,
+    @required this.accountNumber,
+  });
 }
