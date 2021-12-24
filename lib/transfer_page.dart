d718925932b79813e49b9a1853509543c2dda758
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-12-24 14:25:58 -0300

    :bug: fix overflowed

--- lib/transfer_page.dart
+++ lib/transfer_page.dart
@@ -19,62 +19,64 @@ class TransferPage extends StatelessWidget {
       appBar: AppBar(title: Text('Transferência')),
       body: Container(
         padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              CustomTextFormField(
-                prefixIcon: Icon(Icons.account_balance),
-                labelText: 'Conta',
-                hintText: '000000',
-                validator: (String value) {
-                  if (value == null || value == '') {
-                    return 'Informe o número da conta';
-                  }
-                  return null;
-                },
-                onSaved: (String value) {
-                  return _transferModel.accountNumber = int.tryParse(value);
-                },
-                keyboardType: TextInputType.number,
-              ),
-              CustomTextFormField(
-                prefixIcon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0.00',
-                keyboardType: TextInputType.number,
-                validator: (String value) {
-                  if (value == '' || value == null) {
-                    return 'Informe o valor';
-                  }
-                  return null;
-                },
-                onSaved: (String value) {
-                  return _transferModel.value = double.tryParse(value);
-                },
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                      EdgeInsets.all(16.0),
-                    ),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                CustomTextFormField(
+                  prefixIcon: Icon(Icons.account_balance),
+                  labelText: 'Conta',
+                  hintText: '000000',
+                  validator: (String value) {
+                    if (value == null || value == '') {
+                      return 'Informe o número da conta';
+                    }
+                    return null;
+                  },
+                  onSaved: (String value) {
+                    return _transferModel.accountNumber = int.tryParse(value);
+                  },
+                  keyboardType: TextInputType.number,
+                ),
+                CustomTextFormField(
+                  prefixIcon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0.00',
+                  keyboardType: TextInputType.number,
+                  validator: (String value) {
+                    if (value == '' || value == null) {
+                      return 'Informe o valor';
+                    }
+                    return null;
+                  },
+                  onSaved: (String value) {
+                    return _transferModel.value = double.tryParse(value);
+                  },
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(16.0),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
                       ),
                     ),
+                    onPressed: () => _createsTransfer(
+                      formKey: _formKey,
+                      transferModel: _transferModel,
+                      context: context,
+                    ),
+                    child: Text('Transferir'),
                   ),
-                  onPressed: () => _createsTransfer(
-                    formKey: _formKey,
-                    transferModel: _transferModel,
-                    context: context,
-                  ),
-                  child: Text('Transferir'),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -88,7 +90,7 @@ class TransferPage extends StatelessWidget {
   }) {
     if (formKey.currentState.validate()) {
       formKey.currentState.save();
-      
+
       Navigator.pop(context, transferModel);
     }
   }
